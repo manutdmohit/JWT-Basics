625cd4b12a5ebdf01c0b70ea402eef2b17f927fe
manutdmohit <saudmohit@gmail.com>
2022-02-21 21:27:39 +0545

    Status Codes

--- errors/bad-request.js
+++ errors/bad-request.js
@@ -1,9 +1,11 @@
+const { StatusCodes } = require('http-status-codes');
+
 const CustomAPIError = require('./custom-error');
 
 class BadRequest extends CustomAPIError {
   constructor(message) {
     super(message);
-    this.statusCode = 400;
+    this.statusCode = StatusCodes.BAD_REQUEST;
   }
 }
 
--- errors/unauthenticated.js
+++ errors/unauthenticated.js
@@ -1,9 +1,10 @@
+const { StatusCodes } = require('http-status-codes');
 const CustomAPIError = require('./custom-error');
 
 class UnauthenticatedError extends CustomAPIError {
   constructor(message) {
     super(message);
-    this.statusCode = 401;
+    this.statusCode = StatusCodes.UNAUTHORIZED;
   }
 }
 
